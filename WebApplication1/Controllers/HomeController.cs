91b6f60cc8dd63b94d829aef780f81451447210a
kacper.smaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-10-08 16:48:34 +0200

    age

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,6 +12,18 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    
+    /*
+     * Zadanie 1
+     * Zdefiniuj metodę Age, która na podstawie dwóch data w parametrach: birth, future
+     * obliczy wiek osoby w roku future
+     * Uwzględnij pełne lata
+     */
+    
+    public IActionResult About()
+    {
+        return View();
+    }
 
     public IActionResult Index()
     {
@@ -20,6 +32,82 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
+
+        return View();
+    }
+    public IActionResult Age(DateTime birth, DateTime future)
+    {
+        //Future pozniejsze od birth
+        ViewBag.birth = birth;
+        ViewBag.future = future;
+
+
+        
+        if (birth >  future)
+        {
+            ViewBag.ErrorMessage = "Niepoprawna data!";
+            return View("CustomError");
+        }
+
+        int age = future.Year - birth.Year;
+
+        if (birth.Month < future.Month)
+            age--;
+        else if(birth.Day < future.Day)
+        {
+            age--;
+        }
+
+        ViewBag.Result = age;
+
+        return View();
+    }
+    
+    public IActionResult Calculator(Operator? op, double? a, double? b)
+    {
+        // var op = Request.Query["op"];
+        // var a = double.Parse(Request.Query["a"]);
+        // var b = double.Parse(Request.Query["b"]);
+        if (a is null || b is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze a lub b!";
+            return View("CustomError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CustomError");
+        }
+        ViewBag.A = a;
+        ViewBag.B = b;
+        ViewBag.Operator = op;
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.Result = a + b;
+                ViewBag.Operator = "+";
+                break;
+            case Operator.Sub :
+                ViewBag.Result = a - b;
+                ViewBag.Operator = "-";
+                break;
+            case Operator.Div:
+                ViewBag.Result = a / b;
+                ViewBag.Operator = ":";
+                break;
+            case Operator.Mul:
+                ViewBag.Result = a * b;
+                ViewBag.Operator = "*";
+                break;
+            default:
+                ViewBag.ErrorMessage = "Nieznany operator!";
+                return View("CustomError");
+        }
+        
+        
+        
+        // ViewBag.Result = 1234;
         return View();
     }
 
@@ -28,4 +116,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public enum Operator
+{
+    Add, Sub, Mul, Div
 }
