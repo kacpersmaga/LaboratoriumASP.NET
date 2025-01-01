e92768c3a47ba5c06fe585acd043dac782523e6c
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2025-01-01 21:39:08 +0100

    add: ProductionCompanyViewModel to display production company details

---
+++ ProjektZaliczeniowy/Models/ViewModels/ProductionCompanyViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjektZaliczeniowy.Models.ViewModels;
+
+public class ProductionCompanyViewModel
+{
+    public string CompanyName { get; set; }
+    public int MovieCount { get; set; }
+    public decimal TotalBudget { get; set; }
+    public int CompanyId { get; set; }
+}
