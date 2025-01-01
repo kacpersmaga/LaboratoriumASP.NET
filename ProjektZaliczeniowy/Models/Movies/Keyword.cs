e255473fbf82a3bf67919cc1b11af4ec6ef2d244
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2025-01-01 21:00:33 +0100

    feat: scaffold initial database

---
+++ ProjektZaliczeniowy/Models/Movies/Keyword.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjektZaliczeniowy.Models.Movies;
+
+public partial class Keyword
+{
+    public int KeywordId { get; set; }
+
+    public string? KeywordName { get; set; }
+}
