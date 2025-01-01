b0d32531dc472731408143a7f4a79e6dc05fc90a
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2025-01-01 21:41:10 +0100

    add: ProductionCompanies action in MovieController for listing production companies

--- ProjektZaliczeniowy/Controllers/MovieController.cs
+++ ProjektZaliczeniowy/Controllers/MovieController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProjektZaliczeniowy.Models.Movies;
+using ProjektZaliczeniowy.Models.Paging;
+using ProjektZaliczeniowy.Models.ViewModels;
 
 namespace ProjektZaliczeniowy.Controllers
 {
@@ -16,6 +18,36 @@ namespace ProjektZaliczeniowy.Controllers
         public MovieController(MoviesContext context)
         {
             _context = context;
+        }
+        
+        public IActionResult ProductionCompanies(int page = 1, int size = 20)
+        {
+            return View(PagingListAsync<ProductionCompanyViewModel>.Create(
+                (p, s) =>
+                    _context.ProductionCompanies
+                        .OrderBy(c => c.CompanyName)
+                        .Skip((p - 1) * s)
+                        .Take(s)
+                        .Select(company => new ProductionCompanyViewModel
+                        {
+                            CompanyId = company.CompanyId,
+                            CompanyName = company.CompanyName ?? "Unknown Company",
+                            MovieCount = _context.MovieCompanies.Count(mc => mc.CompanyId == company.CompanyId),
+                            TotalBudget = _context.MovieCompanies
+                                .Where(mc => mc.CompanyId == company.CompanyId)
+                                .Join(
+                                    _context.Movies,
+                                    mc => mc.MovieId,
+                                    m => m.MovieId,
+                                    (mc, m) => m.Budget ?? 0
+                                )
+                                .Sum()
+                        })
+                        .AsAsyncEnumerable(),
+                _context.ProductionCompanies.Count(),
+                page,
+                size
+            ));
         }
         
     }
